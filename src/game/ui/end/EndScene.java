02b6aff8d44c9c180dc2d1b07daeba2c33b4c4fe
safargergo <gergo.safar@edu.bme.hu>
2022-05-16 12:56:53 +0200

    Kommentezés 2

--- src/game/ui/end/EndScene.java
+++ src/game/ui/end/EndScene.java
@@ -4,7 +4,14 @@ import game.ui.SceneLauncher;
 
 import javax.swing.*;
 
+/**
+ * JFrame which will contain a JPanel
+ */
 public class EndScene extends JFrame{
+    /**
+     * JFrames constructor that sets up the window's size and operations
+     * @param sl SceneLauncher to change the game's states
+     */
     public EndScene(SceneLauncher sl){
         this.setTitle("End Scene");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/game/ui/end/WinnerPanel.java
+++ src/game/ui/end/WinnerPanel.java
@@ -6,19 +6,35 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Line2D;
 
+/**
+ * JPanel that writes out the winners name
+ */
 public class WinnerPanel extends JPanel {
 
     private String winner;
+
+    /**
+     * constructor of WinnerPanel
+     * @param winner
+     */
     public WinnerPanel(String winner){
         this.winner=winner;
         this.setVisible(true);
     }
 
+    /**
+     * Paint function that is called by system
+     * @param g
+     */
     @Override
     public void paint(Graphics g) {
         drawOutWinner(g);
     }
 
+    /**
+     * Method that draws out the winners name
+     * @param g
+     */
     private void drawOutWinner(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
